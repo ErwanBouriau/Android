1a647983ac0144b7d86c6ed3c309fec1793c9c46
Remy Jacquet <jacquetr@iut1i-702-a16.local>
2019-11-28 11:06:53 +0100

    poubelle

--- app/src/main/java/com/example/fruit_tourney/MainActivity.java
+++ app/src/main/java/com/example/fruit_tourney/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.fruit_tourney;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.firebase.ui.auth.AuthUI;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    Button button = (Button) findViewById(R.id.bouton_connexion);
+
+    //FOR DATA
+    // 1 - Identifier for Sign-In Activity
+    private static final int RC_SIGN_IN = 123;
+
+    // --------------------
+    // ACTIONS
+    // --------------------
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSignInActivity();
+            }
+        });
     }
+
+
+    public void onClickLoginButton() {
+        // 3 - Launch Sign-In Activity when user clicked on Login Button
+        this.startSignInActivity();
+    }
+
+
+    // --------------------
+    // NAVIGATION
+    // --------------------
+
+    // 2 - Launch Sign-In Activity
+    private void startSignInActivity(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build()))
+                        .setIsSmartLockEnabled(false, true)
+                        .setLogo(R.mipmap.ic_launcher)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
 }
