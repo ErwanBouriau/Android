8e84a1fae684efde6d2a92b4645d8200799b51da
Erwan Bouriau <bouriaue@iut1i-702-a15.local>
2019-11-28 13:57:05 +0100

    starting page

--- app/src/main/java/com/example/fruit_tourney/LoginPage.java
+++ app/src/main/java/com/example/fruit_tourney/LoginPage.java
@@ -2,15 +2,16 @@ package com.example.fruit_tourney;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.pm.ActivityInfo;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_login_page);
     }
 }
