5729028a23995c449f9569ee9028a7b3508d5138
Remy Jacquet <jacquetr@iut1i-702-a16.local>
2019-11-28 13:58:55 +0100

    auth

--- app/src/main/java/com/example/fruit_tourney/LoginPage.java
+++ app/src/main/java/com/example/fruit_tourney/LoginPage.java
@@ -2,15 +2,51 @@ package com.example.fruit_tourney;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+
 
 public class LoginPage extends AppCompatActivity {
+
+    //FOR DATA
+    // 1 - Identifier for Sign-In Activity
+    private static final int RC_SIGN_IN = 123;
+
+    // --------------------
+    // ACTIONS
+    // --------------------
+
+    public void onClickLoginButton(View v) {
+        // 3 - Launch Sign-In Activity when user clicked on Login Button
+        this.startSignInActivity();
+    }
+
+
+    // --------------------
+    // NAVIGATION
+    // --------------------
+
+    // 2 - Launch Sign-In Activity
+    private void startSignInActivity(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.AppTheme)
+                        .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build()))
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_login_page);
     }
 }
