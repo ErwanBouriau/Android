cf9977d025cd4a2f1c7a84c6e3c3518ebfbcbfc9
Remy Jacquet <jacquetr@iut1i-702-a16.local>
2019-11-28 16:16:52 +0100

    auth reussi avec deco

--- app/src/main/java/com/example/fruit_tourney/LoginPage.java
+++ app/src/main/java/com/example/fruit_tourney/LoginPage.java
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.os.Bundle;
-import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Arrays;
 
@@ -25,7 +26,19 @@ public class LoginPage extends AppCompatActivity {
 
     public void onClickLoginButton(View v) {
         // 3 - Launch Sign-In Activity when user clicked on Login Button
-        this.startSignInActivity();
+        if(FirebaseAuth.getInstance().getCurrentUser() != null) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+        else {
+            this.startSignInActivity();
+        }
+    }
+
+    public void onClickLogoutButton(View v) {
+        FirebaseAuth.getInstance().signOut();
+        finish();
+        startActivity(new Intent(this, LoginPage.class));
     }
 
 
@@ -52,5 +65,18 @@ public class LoginPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login_page);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(FirebaseAuth.getInstance().getCurrentUser() != null) {
+            Button bouttonLogin = (Button) this.findViewById(R.id.btn_cnx);
+            bouttonLogin.setText("JOUER !");
+            TextView texte = (TextView) this.findViewById(R.id.texte_co);
+            texte.setText(FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
+            Button bouttonLogout = (Button) this.findViewById(R.id.btn_deco);
+            bouttonLogout.setVisibility(View.VISIBLE);
+        }
     }
 }
