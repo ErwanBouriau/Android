f54b803b6cf289a9908e201e7e90a94a71172c26
Remy Jacquet <jacquetr@iut1i-702-a16.local>
2019-12-06 11:12:18 +0100

    bdd + affichage

--- app/src/main/java/com/example/fruit_tourney/TournoiActivity.java
+++ app/src/main/java/com/example/fruit_tourney/TournoiActivity.java
@@ -1,9 +1,20 @@
 package com.example.fruit_tourney;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+
 
 public class TournoiActivity extends AppCompatActivity {
 
@@ -12,7 +23,37 @@ public class TournoiActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tournoi);
 
-        String tab[] = new String[20];
-        tab = ["Banane.png"];
+        ArrayList<StorageReference> allReferences = new ArrayList<StorageReference>();
+        initialize(allReferences);
+
+//        ImageView image1 = this.findViewById(R.id.fruit1);
+//        GlideApp.with(this)
+//                .load(allReferences.get(0))
+//                .into(image1);
+//        ImageView image2 = this.findViewById(R.id.fruit2);
+//        GlideApp.with(this)
+//                .load(allReferences.get(1))
+//                .into(image2);
+
+        System.out.println(allReferences);
     }
+
+    public void initialize(final ArrayList<StorageReference> refs) {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+        storageRef.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
+            @Override
+            public void onSuccess(ListResult listResult) {
+                for (StorageReference image : listResult.getItems()) {
+                    refs.add(image);
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                System.out.println("listeAll failed !");
+            }
+        });
+    }
+
 }
